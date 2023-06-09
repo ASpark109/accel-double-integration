2c8dfa02f9b58b7d41de31b02a6cfccd04202d37
KLiubomyr <applecoms123@gmail.com>
2023-06-09 23:09:13 +0300

    Updata inertsys.py
    Structured code, added processing of y and z axes.

--- src/inertsys.py
+++ src/inertsys.py
@@ -12,12 +12,19 @@ COLOR = (200, 200, 200)
 
 freq = 1/30 #Hz
 
-def draw_window(x):
-    WIN.fill((0, 0, 0))
+def draw_window(coordinate):
+    
+    run = True
 
-    pygame.draw.rect(WIN, COLOR, (x*100 + 500, 500, 5, 5))
-
-    pygame.display.update()
+    for i in range(coordinate.shape[1]):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    return 0
+        pygame.time.Clock().tick(50)
+        # WIN.fill((0, 0, 0))
+        pygame.draw.rect(WIN, COLOR, (coordinate[0][i]* 200 + 1000, 500 - coordinate[1][i]* 200, 5, 5))
+        pygame.display.update()
 
 def savitzky_golay_filter(data, window_size, polyorder):
     filtered_data = savgol_filter(data, window_size, polyorder)
@@ -37,16 +44,16 @@ def kalman_filter(data):
     filtered_data = kf.em(data).smooth(data)[0]
     return filtered_data
 
-def integral(data, freq, p):
+def integral(data, freq):
     vel = 0
 
-    out = [[0,0]]
+    out = [0]
 
     for i in range(len(data)-1):
-        I = integrate.quad(integrand, 0, freq, args=f(data[i][p], data[i+1][p], freq))
+        I = integrate.quad(integrand, 0, freq, args=f(data[i], data[i+1], freq))
         vel += I[0]
 
-        out.append([i+1, vel])
+        out.append(vel)
     
     return np.array(out)
 
@@ -55,35 +62,52 @@ def read_data(file_name):
     data = csv.reader(file)
 
     next(data)
-    acc = np.array([[0,0]])
+    acc = np.array([[0,0,0]])
 
     for r in data:
-        d = np.array([[float(r[0].split(";")[1]),float(r[0].split(";")[2])]])
+        d = np.array([[float(r[0].split(";")[1]), float(r[0].split(";")[2]) * 10, float(r[0].split(";")[3]) * 10 ]])
         acc = np.r_[acc, d]
     
     return acc
 
-def main():
+def get_displacement(accel):
 
-    accel = read_data("filter.csv")
+    data = [0,0,0]
 
-    filtred_accel = np.reshape(savitzky_golay_filter(accel[:, 0], 3, 1), (-1,1))
+    for i in range(3):
+        filtred_accel = savitzky_golay_filter(accel[:, i], 3, 1) #x
+        velocity = integral(filtred_accel, freq)
+        displacement = integral(velocity, freq)
 
-    velocity = integral(filtred_accel, freq, 0)
-    displacement = integral(velocity, freq, 1)
+        data[i] = displacement
 
+        if i == 1:
+            plot_data(accel, filtred_accel, velocity, displacement)
 
-    plt.plot(displacement[:, 0], displacement[:, 1], color='r', label='s')
-    plt.plot(velocity[:, 0], velocity[:, 1], color='g', label='v')
-    plt.plot(accel[:, 0], color='b', label='a')
-    plt.plot(filtred_accel, color='y', label='filtered')
+    return np.array(data)
+
+def plot_data(accel, filtred_accel, velocity, displacement):
+    a = integrate.cumtrapz(accel[:, 1], None, freq,initial=0)
+
+    plt.plot(a, color='black', label='velocity new')
+    plt.plot(displacement, color='r', label='displacement')
+    plt.plot(velocity, color='b', label='velocity')
+    plt.plot(accel[:, 1], color='g', label='accel')
+    plt.plot(filtred_accel, color='y', label='filtred accel')
     plt.title('Графік швидкості/Графік переміщення')
-    plt.xlabel('x-axis')
-    plt.ylabel('y-axis')
+    plt.xlabel('time')
+    plt.ylabel('m/ms^2/(m/s)')
     plt.legend()
     plt.grid()
     plt.show()
 
+def main():
+
+    accel = read_data("Aaa.csv")
+
+    coordinate = get_displacement(accel)
+
+    draw_window(coordinate)
 
 
 if __name__ == "__main__":
