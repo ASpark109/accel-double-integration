04f2d2a23cd64e6cc94a5613b85793843855805e
KLiubomyr <applecoms123@gmail.com>
2023-06-11 02:42:28 +0300

    Some changes
    The test has been updated to demonstrate the algorithm's operation. Internal class variables have been made private. Validation of input data (partially error handling). The ability to set the punctuation mark when reading a csv file.

--- src/accdblinteg.py
+++ src/accdblinteg.py
@@ -1,6 +1,5 @@
 #Обробка помилок
-    #Якщо файл не існує
-    #Якщо step задано невірне число
+    #Коректне читання даних
     #Інтегрування пустих данних
 
 import numpy as np
@@ -8,72 +7,110 @@ import csv
 from scipy import integrate
 import matplotlib.pyplot as plt
 import pygame
+import sys
+from colorama import init
+from colorama import Fore, Style
 
 class dblinteg:
 
-    step = 0
-    input_data = None
+    def __init__(self):
+        init()
+        self.__private_step = 1
+        self.__private_input_data = None
 
-    velocity = None
-    displacement = None
+        self.__private_velocity = None
+        self.__private_displacement = None
+
+        self.__private_separator = ','
+        self.__private__visualise_scale = 1
 
     def set_step(self, n):
-        self.step = n
+
+        if n > 0:
+            self.__private_step = n
+        else:
+            print(Fore.YELLOW + Style.BRIGHT + "[Warning]" + Style.RESET_ALL + ": The number must be greater than zero the step is set to 1")
 
     def get_step(self):
-        return self.step
+        return self.__private_step
     
     def get_input_data(self):
-        return self.input_data
+        return self.__private_input_data
     
     def get_integrated_data(self):
-        return self.displacement
+        return self.__private_displacement
     
     def get_velocity(self):
-        return self.velocity
+        return self.__private_velocity
     
+    def get_separator(self):
+        return self.__private_separator
+    
+    def get_scale(self):
+        return self.__private__visualise_scale
+    
+    def set_scale(self, n):
+
+        if n > 0:
+            self.__private_separator = n
+        else:
+            print(Fore.YELLOW + Style.BRIGHT + "[Warning]" + Style.RESET_ALL + ": The number must be greater than zero the scale is set to 1")
+    
+    def set_separator(self, c):
+
+        if len(c) == 1:
+            self.__private_separator = c
+        else:
+            print(Fore.YELLOW + Style.BRIGHT + "[Warning]" + Style.RESET_ALL + ": A separator character can consist of only one character. Set to ;")
+
     def readf(self, file_name):
 
-        file = open(file_name)
-        rdata = csv.reader(file)
+        try:
+            file = open(file_name)
+        except FileNotFoundError:
+            msg = Fore.RED + Style.BRIGHT + "[ERROR]" + Style.RESET_ALL + ": Sorry, the file "+ file_name + " does not exist."
+            print(msg)   
+            sys.exit(1) 
+
+        rdata = csv.reader(file, delimiter = self.__private_separator)
 
         next(rdata)
         acc = np.array([[0,0,0]])
 
         for r in rdata:
-            d = np.array([[float(r[0].split(";")[1]), float(r[0].split(";")[2]), float(r[0].split(";")[3])]])
+            d = np.array([[float(r[1]), float(r[2]), float(r[3])]])
             acc = np.r_[acc, d]
 
-        self.input_data = acc
+        self.__private_input_data = acc
     
     def dblintegral(self):
 
-        disp_data = [0,0,0]
-        vel_data = [0,0,0]
+        disp_data = []
+        vel_data = []
 
         for i in range(3):
-            vel_data[i] = self.integral(self.input_data[:, i])
-            disp_data[i] = self.integral(vel_data[i])
+            vel_data.append(self.__private_integral(self.__private_input_data[:, i]))
+            disp_data.append(self.__private_integral(vel_data[i]))
 
-        self.velocity = np.array(vel_data)
-        self.displacement = np.array(disp_data)
+        self.__private_velocity = np.array(vel_data)
+        self.__private_displacement = np.array(disp_data)
     
-    def integrand(self, x, k, b):
+    def __private_integrand(self, x, k, b):
         return k*x + b
     
-    def f(self, a, b):
-        kx = (b - a) / self.step
-        bx = a
+    def __private_f(self, a, b):
+        k = (b - a) / self.__private_step
+        b = a
 
-        return ((kx, bx))
+        return ((k, b))
 
-    def integral(self, data):
+    def __private_integral(self, data):
         vel = 0
 
         out = [0]
 
         for i in range(len(data)-1):
-            I = integrate.quad(self.integrand, 0, self.step, args=self.f(data[i], data[i+1]))
+            I = integrate.quad(self.__private_integrand, 0, self.__private_step, args=self.__private_f(data[i], data[i+1]))
             vel += I[0]
 
             out.append(vel)
@@ -84,15 +121,15 @@ class dblinteg:
 
         plt.figure(figsize=(20,10))
         plt.subplot(1, 2, 1)
-        plt.plot(self.velocity[n], color='b', label='velocity (m/s)')
-        plt.plot(self.input_data[:, n], color='g', label='acceleration (m^2/s)')
+        plt.plot(self.__private_velocity[n], color='b', label='velocity (m/s)')
+        plt.plot(self.__private_input_data[:, n], color='g', label='acceleration (m^2/s)')
         plt.xlabel('time')
         plt.legend()
         plt.grid()
         plt.title('Acceleration/Velocity')
         
         plt.subplot(1, 2, 2)
-        plt.plot(self.displacement[1], color='orange', label='displacement (m)')
+        plt.plot(self.__private_displacement[n], color='orange', label='displacement (m)')
         plt.xlabel('time')
         plt.legend()
         plt.grid()
@@ -105,16 +142,16 @@ class dblinteg:
         WIDTH = 2340
         HEIGHT = 780
 
-        scale = 10
+        scale = 1
 
         WIN = pygame.display.set_mode((WIDTH, HEIGHT))
         
-        for i in range(self.displacement.shape[1]):
+        for i in range(self.__private_displacement.shape[1]):
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
                         return 0
-            pygame.time.Clock().tick(50)
+            pygame.time.Clock().tick(90)
             WIN.fill((0, 0, 0))
-            pygame.draw.rect(WIN, 'white', (self.displacement[0][i]*scale + WIDTH/2, HEIGHT/2 - self.displacement[1][i]*scale, 5, 5))
+            pygame.draw.rect(WIN, 'white', (self.__private_displacement[0][i]*scale + WIDTH/2, HEIGHT/2 - self.__private_displacement[1][i]*scale, 5, 5))
             pygame.display.update()
